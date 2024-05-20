d644fa7b7c6dca047ff835d9f177088bd74ba039
Conner Hufnagel <conner.huf@gmail.com>
2024-05-20 15:46:35 -0700

    Update 14-3Sum.py

--- Problems/14-3Sum.py
+++ Problems/14-3Sum.py
@@ -2,6 +2,12 @@
 
 # -------------------------------------------------------------------- #
 
+'''
+This solution uses a two-pointer approach to find all the unique triplets that sum to zero. We first sort the input list. We then iterate through the list, using the current value as the first value in the triplet. We then use two pointers to find the other two values in the triplet. We skip over duplicate values to avoid duplicate triplets. If the sum of the three values is greater than zero, we decrement the right pointer. If the sum is less than zero, we increment the left pointer. If the sum is zero, we add the triplet to the result list. We then increment the left pointer and skip over duplicate values. We continue this process until the left pointer is greater than the right pointer.
+'''
+
+from typing import List
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         result = []
